b95183a109ed03fb6eb578ad77db33f4953ce682
Niclas <niclastanskanen@hotmail.com>
2024-11-12 13:04:52 +0100

    Add styling to header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="text-primary p-4 flex justify-center border-b">
+      <h1 className="text-3xl font-bold">Crypto Price Tracker</h1>
+    </header>
+  );
 };
 
 export default Header;
