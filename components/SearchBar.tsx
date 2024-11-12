de3594bcc7426408973eca4f22d2ac9cd19801e3
Niclas <niclastanskanen@hotmail.com>
2024-11-12 13:08:59 +0100

    Add styling to SearchBar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,16 @@
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="flex items-center space-x-2 mb-4">
+      <Input type="text" placeholder="Search cryptocurrencies..." />
+      <Button size="icon">
+        <Search className="h-4 w-4" />
+      </Button>
+    </div>
+  );
 };
 
 export default SearchBar;
