178c1f3baf98cfee4f6eff13df8c8427f8136e40
Niclas <niclastanskanen@hotmail.com>
2024-11-13 08:16:05 +0100

    add styling to CryptoList component

--- components/CryptoList.tsx
+++ components/CryptoList.tsx
@@ -1,5 +1,30 @@
+import CryptoItem from "./CryptoItem";
+
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const CryptoList = () => {
-  return <div>CryptoList</div>;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Symbol</TableHead>
+          <TableHead className="text-right">Price (USD)</TableHead>
+          <TableHead className="text-right">24h Change</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <CryptoItem />
+      </TableBody>
+    </Table>
+  );
 };
 
 export default CryptoList;
