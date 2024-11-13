5d905c8cbe5cd16ddf8d103febb0c221652e5426
Niclas <niclastanskanen@hotmail.com>
2024-11-13 08:16:19 +0100

    add styling to FavoritesList component

--- components/FavoritesList.tsx
+++ components/FavoritesList.tsx
@@ -1,5 +1,49 @@
+import { Star } from "lucide-react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
 const FavoritesList = () => {
-  return <div>FavoritesList</div>;
+  return (
+    <Card className="mt-4">
+      <CardHeader>
+        <CardTitle>Favorites</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead className="text-right">Price (USD)</TableHead>
+              <TableHead className="text-right">24h Change</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">
+                <div className="flex items-center">
+                  <span className="mr-2">Crypto Name</span>
+                  <Button variant="ghost" size="icon">
+                    <Star className="h-4 w-4 fill-yellow-400" />
+                  </Button>
+                </div>
+              </TableCell>
+              <TableCell className="text-right">price USD</TableCell>
+              <TableCell className="text-right">% change</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default FavoritesList;
