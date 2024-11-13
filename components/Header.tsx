40bdd6dfe5e724b5d986931a915031a15d757dd2
Niclas <niclastanskanen@hotmail.com>
2024-11-13 10:59:21 +0100

    refactor Header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="text-primary p-4 flex justify-center border-b mb-4">
+    <header className="text-primary p-4 flex justify-center border-b">
       <h1 className="text-3xl font-bold">Crypto Price Tracker</h1>
     </header>
   );
