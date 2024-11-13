41768d80bf9fbe18978b1ae0a7812f404128928d
Niclas <niclastanskanen@hotmail.com>
2024-11-13 11:06:54 +0100

    refactor ErrorBoundary to accept error props and styling

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,5 +1,19 @@
-const ErrorBoundary = () => {
-  return <div>ErrorBoundary</div>;
+interface ErrorBoundaryProps {
+  error: string | null;
+}
+
+const ErrorBoundary = ({ error }: ErrorBoundaryProps) => {
+  if (!error) return;
+
+  return (
+    <div
+      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      role="alert"
+    >
+      <strong className="font-bold">Error: </strong>
+      <span className="block sm:inline">{error}</span>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
